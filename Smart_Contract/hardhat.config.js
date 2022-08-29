70b7009506165918ac00e4297fcabadd03c28faf
codewithrt <97937663+codewithrt@users.noreply.github.com>
2022-08-29 23:13:46 +0530

    Update hardhat.config.js

--- Smart_Contract/hardhat.config.js
+++ Smart_Contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks:{
     ganache:{
       url:'HTTP://127.0.0.1:7545',
-      accounts:['e7b0998ed459c79bedd810cbc52689e5f72ef855d236c5224cbe2ead33c7b414']
+      accounts:['']
     }
   }
 };
